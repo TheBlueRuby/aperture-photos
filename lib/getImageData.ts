b402a0a30f4656a69074e1dc52d7faff3ad36fab
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-08-30 23:18:04 +1000

    cleaned up image loading

--- lib/getImageData.ts
+++ lib/getImageData.ts
@@ -1,75 +1,66 @@
 import exifr from "exifr";
 
 type CameraData = {
-  Model: string;
-  Lens: string;
-  ApertureValue: string;
-  ShutterSpeed: string;
-  ISO: string;
-  FocalLength: string;
+	Model: string;
+	Lens: string;
+	ApertureValue: string;
+	ShutterSpeed: string;
+	ISO: string;
+	FocalLength: string;
 };
 function cameraData(
-  Model?: string,
-  Lens?: string,
-  ApertureValue?: string,
-  ShutterSpeed?: string,
-  ISO?: string,
-  FocalLength?: string
+	Model?: string,
+	Lens?: string,
+	ApertureValue?: string,
+	ShutterSpeed?: string,
+	ISO?: string,
+	FocalLength?: string
 ): CameraData {
-  let ret = {
-    Model: "Unknown",
-    Lens: "Unknown",
-    ApertureValue: "Unknown",
-    ShutterSpeed: "Unknown",
-    ISO: "Unknown",
-    FocalLength: "Unknown",
-  };
-  if (Model != undefined) {
-    ret.Model = Model;
-  }
-  if (Lens != undefined) {
-    ret.Lens = Lens;
-  }
-  if (ApertureValue != undefined) {
-    ret.ApertureValue = "f" + ApertureValue;
-  }
-  if (ShutterSpeed != undefined) {
-    ret.ShutterSpeed = "1/" + Math.round(1 / parseFloat(ShutterSpeed));
-  }
-  if (ISO != undefined) {
-    ret.ISO = ISO;
-  }
-  if (FocalLength != undefined) {
-    ret.FocalLength = FocalLength + "mm";
-  }
-  return ret;
+	let ret = {
+		Model: "Unknown",
+		Lens: "Unknown",
+		ApertureValue: "Unknown",
+		ShutterSpeed: "Unknown",
+		ISO: "Unknown",
+		FocalLength: "Unknown",
+	};
+	if (Model != undefined) {
+		ret.Model = Model;
+	}
+	if (Lens != undefined) {
+		ret.Lens = Lens;
+	}
+	if (ApertureValue != undefined) {
+		ret.ApertureValue = "f" + ApertureValue;
+	}
+	if (ShutterSpeed != undefined) {
+		ret.ShutterSpeed = "1/" + Math.round(1 / parseFloat(ShutterSpeed));
+	}
+	if (ISO != undefined) {
+		ret.ISO = ISO;
+	}
+	if (FocalLength != undefined) {
+		ret.FocalLength = FocalLength + "mm";
+	}
+	return ret;
 }
 
 export default async function getImageData(
-  imageId: string
+	imageId: string
 ): Promise<CameraData> {
-  return exifr
-    .parse(
-      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${imageId}`
-    )
-    .then(
-      (output) => {
-        if (output != undefined) {
-          return cameraData(
-            output.Make + " " + output.Model,
-            output.Lens,
-            output.FNumber,
-            output.ExposureTime,
-            output.ISO,
-            output.FocalLengthIn35mmFormat
-          );
-        } else {
-          return cameraData();
-        }
-      },
-      (error) => {
-        console.error(error);
-        return cameraData();
-      }
-    );
+	let output = await exifr.parse(
+		`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${imageId}`
+	);
+	if (output == null || output == undefined) {
+		return cameraData();
+	}
+	
+	return cameraData(
+		output.Make + " " + output.Model,
+		output.Lens,
+		output.FNumber,
+		output.ExposureTime,
+		output.ISO,
+		output.FocalLengthIn35mmFormat
+	);
 }
