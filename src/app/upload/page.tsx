f95b3119f762aec5dee8c72b4c4dd3d12c35228b
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-09-04 14:43:02 +1000

    fixed another build issue hopefully

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,9 +3,8 @@ import UploadArea from "./UploadArea";
 
 import { createClient } from "@/supabase/server";
 
-const supabase = createClient();
-
 export default async function Page() {
+	const supabase = createClient();
 	const { data, error } = await supabase.auth.getUser();
 	return (
 		<div className={styles.uploadContainer}>
