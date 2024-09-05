ba4fccc34b0b246be6f7c343d1a5727f4fee956e
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2024-09-05 13:24:30 +1000

    hopefully fixed signup logic

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -1,46 +1,67 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
-import { createClient } from '@/supabase/server'
+import { createClient } from "@/supabase/server";
 
 export async function login(formData: FormData) {
-  const supabase = createClient()
+	const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
 
-  const { error } = await supabase.auth.signInWithPassword(data)
+	const { error } = await supabase.auth.signInWithPassword(data);
 
-  if (error) {
-    redirect('/error')
-  }
+	if (error) {
+		redirect("/error");
+	}
 
-  revalidatePath('/', 'layout')
-  redirect('/')
+	revalidatePath("/", "layout");
+	redirect("/");
 }
 
 export async function signup(formData: FormData) {
-  const supabase = createClient()
+	const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
 
-  const { error } = await supabase.auth.signUp(data)
+	const { data: signUpData, error } = await supabase.auth.signUp(data);
 
-  if (error) {
-    redirect('/error')
-  }
+	if (error) {
+		redirect("/error");
+	}
 
-  revalidatePath('/', 'layout')
-  redirect('/')
+	if (
+		await supabase
+			.from("profiles")
+			.select("username")
+			.eq("username", formData.get("username") as string)
+			.then((data) => {
+				if (data.data == undefined) {
+					return false;
+				}
+				return data.data?.length > 0;
+			})
+	) {
+		redirect("/error");
+	}
+
+	supabase.from("profiles").insert({
+		id: signUpData.user?.id,
+		username: formData.get("username") as string,
+		displayname: formData.get("displayname") as string,
+	});
+
+	revalidatePath("/", "layout");
+	redirect("/");
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,12 @@ export default function LoginPage() {
 	return (
 		<div className={styles.formContainer}>
 			<form className={styles.loginForm}>
+				<label htmlFor="email">Display Name:</label>
+				<input id="displayname" name="displayname" type="text" required />
+				<label htmlFor="username">Username:</label>
+				<input id="username" name="username" type="text" required />
 				<label htmlFor="email">Email:</label>
-				<input id="email" name="email" type="email" required />
+				<input id="email" name="email" type="email" placeholder="name@example.com" required />
 				<label htmlFor="password">Password:</label>
 				<input id="password" name="password" type="password" required />
 				<button className={styles.button} formAction={login}>Log in</button>
